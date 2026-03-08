2f71207eb00d2bc9847cdde6f24b5df28608649b
joaosimsic <jpsimsic@hotmail.com>
2026-03-07 22:45:39 -0300

    feat: quick test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 mod frontend;
 
+use frontend::parse_dsl;
+
 fn main() {
-    println!("Hello, world!");
+    let input_code = r#"
+        state Pendente {
+            on Pagar -> Pago;
+            on Cancelar -> Cancelado;
+        }
+
+        state Pago {
+            data { transaction_id: String, }
+            on Enviar -> Enviado;
+            on Reembolsar -> Reembolsado;
+        }
+
+        state Enviado {
+            terminal; 
+        }
+    "#;
+
+    println!("Compiling DSL tyto... \n");
+
+    match parse_dsl(input_code) {
+        Ok(ast) => {
+            println!("AST generated");
+            println!("{:#?}", ast);
+        }
+        Err(e) => {
+            eprintln!("Syntax error:\n{}", e);
+        }
+    }
 }
