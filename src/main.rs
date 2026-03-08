9b55be4bb41a26cb4c9a089385f2ea184f9362dd
joaosimsic <jpsimsic@hotmail.com>
2026-03-08 02:08:43 -0300

    refac: main file to use cli

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,115 @@
-mod frontend; 
-mod middle; 
+// src/main.rs
 mod backend;
-mod config;
 mod cli;
+mod config;
+mod frontend;
+mod middle;
 
-use frontend::parse_dsl; 
-use middle::{StateGraph, Validator}; 
-use backend::generate_ts;
+use clap::Parser;
+use std::fs;
+use std::path::Path;
+
+use backend::typescript::generate_ts;
+use cli::{Cli, Commands};
+use config::{GlobalConfig, LocalConfig};
+use frontend::parse_dsl;
+use middle::{StateGraph, Validator};
 
 fn main() {
-    let input_code = r#"
-        state Pendente {
-            data { id: String, }
-            on Pagar -> Pago;
-        }
+    let cli = Cli::parse();
 
-        state Pago {
-            data { transaction_id: String, amount: f64, }
-            on Enviar -> Enviado;
-        }
+    match &cli.command {
+        Commands::Build { config, machine } => {
+            println!("Tyto - Starting workspace build...\n");
 
-        state Enviado {
-            terminal; 
-        }
-    "#;
-
-    println!("Compilando DSL Tyto... \n");
-
-    match parse_dsl(input_code) {
-        Ok(ast) => {
-            println!("✅ AST gerada com sucesso.");
-
-            match StateGraph::from_ast(&ast) { 
-                Ok(graph) => {
-                    if let Err(errors) = Validator::validate(&graph) { 
-                        println!("\n❌ Erros de Validação Encontrados:");
-                        for err in errors {
-                            eprintln!("  - {}", err);
-                        }
-                    } else {
-                        println!("✅ Validação semântica aprovada!");
-                        
-                        let ts_code = generate_ts(&ast);
-                        println!("\n📦 Código TypeScript Gerado:\n");
-                        println!("{}", ts_code);
-                    }
+            let global_config = match GlobalConfig::load(config) {
+                Ok(c) => c,
+                Err(e) => {
+                    eprintln!("Error reading global configuration '{}': '{}'", config, e);
+                    std::process::exit(1);
                 }
-                Err(e) => eprintln!("❌ Erro Semântico no Grafo:\n{}", e),
+            };
+
+            let workspace_path = Path::new(&global_config.workspace_dir);
+            if !workspace_path.exists() || !workspace_path.is_dir() {
+                eprintln!(
+                    "The workspace directory '{}' does not exists",
+                    global_config.workspace_dir
+                );
+                std::process::exit(1);
             }
+
+            for entry in fs::read_dir(workspace_path).expect("Error reading workspace_dir") {
+                let entry = entry.unwrap();
+                let path = entry.path();
+
+                if path.is_dir() {
+                    let dir_name = path.file_name().unwrap().to_str().unwrap();
+
+                    if let Some(target_machine) = machine {
+                        if target_machine != dir_name {
+                            continue;
+                        }
+                    }
+
+                    println!("Processing module: [{}]", dir_name);
+
+                    let local_yaml_path = path.join(format!("{}.yaml", dir_name));
+
+                    if !local_yaml_path.exists() {
+                        println!(
+                            "Warning: Configuration '{}' not found. Skipping.",
+                            local_yaml_path.display()
+                        );
+                        continue;
+                    }
+
+                    let local_config =
+                        LocalConfig::load(&local_yaml_path).expect("Error in local YAML file.");
+                    let source_path = path.join(&local_config.source);
+
+                    let source_code = fs::read_to_string(&source_path).unwrap_or_else(|_| {
+                        eprintln!(
+                            "  Error: Source file '{}' not found.",
+                            source_path.display()
+                        );
+                        std::process::exit(1);
+                    });
+
+                    let ast = match parse_dsl(&source_code) {
+                        Ok(tree) => tree,
+                        Err(e) => {
+                            eprintln!("  Syntax error in the DSL module '{}':\n{}", dir_name, e);
+                            std::process::exit(1);
+                        }
+                    };
+
+                    let graph = StateGraph::from_ast(&ast).expect("  Semantic error.");
+
+                    if let Err(errors) = Validator::validate(&graph) {
+                        eprintln!("  Validation Errors in '{}':", dir_name);
+                        for err in errors {
+                            eprintln!("    - {}", err);
+                        }
+                        std::process::exit(1);
+                    }
+
+                    for (lang, target_config) in &local_config.targets {
+                        if lang == "typescript" {
+                            let ts_code = generate_ts(&ast);
+
+                            fs::create_dir_all(&target_config.out_dir).unwrap();
+                            let file_path = Path::new(&target_config.out_dir)
+                                .join(format!("{}_protocol.ts", dir_name));
+
+                            fs::write(&file_path, ts_code).expect("Error saving generated file.");
+                            println!("  TS successfully generated in: {}", file_path.display());
+                        }
+                    }
+                    println!("");
+                }
+            }
+            println!("Workspace build complete!");
         }
-        Err(e) => eprintln!("❌ Erro de sintaxe:\n{}", e), //
     }
 }
