7f0fcdab1a72e8abcb1bc655df899fd8cdd75567
joaosimsic <jpsimsic@hotmail.com>
2026-03-08 20:10:20 -0300

    feat: java

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,19 +1,26 @@
 use crate::frontend::ast::TytoProgram;
 
-pub mod rust;
-pub mod typescript;
-pub mod mermaid;
-
 pub trait Generator {
     fn generate(&self, program: &TytoProgram) -> String;
     fn extension(&self) -> &'static str;
 }
 
-pub fn get_generator(lang: &str) -> Option<Box<dyn Generator>> {
-    match lang {
-        "typescript" => Some(Box::new(typescript::TypeScriptGenerator)),
-        "rust" => Some(Box::new(rust::RustGenerator)),
-        "mermaid" => Some(Box::new(mermaid::MermaidGenerator)),
-        _ => None,
-    }
+macro_rules! register_generators {
+    ($($lang:expr => $module:ident::$generator:ident),* $(,)?) => {
+        $(pub mod $module;)*
+
+        pub fn get_generator(lang: &str) -> Option<Box<dyn Generator>> {
+            match lang {
+                $($lang => Some(Box::new($module::$generator)),)*
+                _ => None,
+            }
+        }
+    };
+}
+
+register_generators! {
+    "typescript" => typescript::TypeScriptGenerator,
+    "rust"       => rust::RustGenerator,
+    "mermaid"    => mermaid::MermaidGenerator,
+    "java"       => java::JavaGenerator,
 }
