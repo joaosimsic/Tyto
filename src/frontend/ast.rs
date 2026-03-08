b23ce0975701ff0f4e10e54a3c1ac21b00e660b1
joaosimsic <jpsimsic@hotmail.com>
2026-03-07 22:09:33 -0300

    feat: ast frontend

---
+++ src/frontend/ast.rs
@@ -0,0 +1,29 @@
+#[derive(Debug, Clone)]
+pub struct TytoProgram {
+    pub states: Vec<State>,
+}
+
+#[derive(Debug, Clone)]
+pub struct State {
+    pub name: String,
+    pub transitions: Vec<Transition>,
+    pub data: Option<DataBlock>,
+    pub is_terminal: bool,
+}
+
+#[derive(Debug, Clone)]
+pub struct Transition {
+    pub event: String,
+    pub target: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct DataBlock {
+    pub fields: Vec<Field>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Field {
+    pub name: String,
+    pub field_type: String,
+}
