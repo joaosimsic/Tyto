f3cb257617103173db7541993a6a450ee1815f47
joaosimsic <jpsimsic@hotmail.com>
2026-03-08 19:25:12 -0300

    feat: mermaid

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -2,6 +2,7 @@ use crate::frontend::ast::TytoProgram;
 
 pub mod rust;
 pub mod typescript;
+pub mod mermaid;
 
 pub trait Generator {
     fn generate(&self, program: &TytoProgram) -> String;
@@ -12,6 +13,7 @@ pub fn get_generator(lang: &str) -> Option<Box<dyn Generator>> {
     match lang {
         "typescript" => Some(Box::new(typescript::TypeScriptGenerator)),
         "rust" => Some(Box::new(rust::RustGenerator)),
+        "mermaid" => Some(Box::new(mermaid::MermaidGenerator)),
         _ => None,
     }
 }
