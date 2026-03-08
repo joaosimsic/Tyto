a22fc217b74efcd536d5ae882b2fa5caab85f279
joaosimsic <jpsimsic@hotmail.com>
2026-03-08 18:57:25 -0300

    feat: generator trait

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,2 +1,17 @@
+use crate::frontend::ast::TytoProgram;
+
 pub mod rust;
 pub mod typescript;
+
+pub trait Generator {
+    fn generate(&self, program: &TytoProgram) -> String;
+    fn extension(&self) -> &'static str;
+}
+
+pub fn get_generator(lang: &str) -> Option<Box<dyn Generator>> {
+    match lang {
+        "typescript" => Some(Box::new(typescript::TypeScriptGenerator)),
+        "rust" => Some(Box::new(rust::RustGenerator)),
+        _ => Nonw,
+    }
+}
